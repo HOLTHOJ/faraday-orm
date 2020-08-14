f67c837303edfd24c0b6fbf80d5b688cfa7dbb09
HOLTHOJ <jeroen.holthof@gmail.com>
2020-08-14 02:45:14 +0200

    Tests

---
+++ test/filesystem/model/DBFile.ts
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2020  Jeroen Holthof <https://github.com/HOLTHOJ/faraday-orm>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+import {Entity} from "../../../src/entity/annotation/Entity";
+import {Callback, CallbackOperation} from "../../../src/entity/annotation/Callback";
+import {UNDEFINED} from "../../../src/util/Undefined";
+import {DBDateConverter} from "../../../src/converter/DBDateConverter";
+import {Internal} from "../../../src/entity/annotation/Internal";
+
+@Entity("file")
+export class DBFile {
+
+    @Internal({name: "ct", converter: DBDateConverter}, true)
+    public createTime: Date = UNDEFINED;
+
+    @Internal({name: "lut", converter: DBDateConverter}, true)
+    public lastUpdateTime: Date = UNDEFINED;
+
+    @Callback()
+    updateTimestamp(operation: CallbackOperation) {
+        if (operation !== "GET" && operation !== "LOAD") {
+            this.lastUpdateTime = new Date();
+            if (operation === "INSERT") {
+                this.createTime = new Date();
+            }
+        }
+    }
+}
